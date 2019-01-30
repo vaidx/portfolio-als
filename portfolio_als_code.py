4a73e745a492ebc89f0cf1e44810681918895ffb
vaibhav <valture66@gmail.com>
2019-01-30 15:23:34 +0800

    basic functiion protoypes added

---
+++ portfolio_als_code.py
@@ -0,0 +1,22 @@
+import time
+
+
+def import_file():
+    print("File import successful")
+
+def sharpeRatio():
+    print("Sharpe Ratio: ")
+
+def sortinoRatio():
+    print("Sortino Ratio: ")
+
+def svm():
+    {}
+
+def arima():
+    {}
+
+def optimizer():
+    {}
+def print_allocations():
+    print("Displaying Portfolio Allocations")
